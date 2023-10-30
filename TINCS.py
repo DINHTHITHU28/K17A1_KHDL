bcfcc0f568c02e0788f4771a27a7bc5d1ef20263
DINHTHITHU28 <148684223+DINHTHITHU28@users.noreply.github.com>
2023-10-30 20:26:46 +0700

    Add files via upload

---
+++ TINCS.py
@@ -0,0 +1,41 @@
+X=6
+Y=7
+C=X+Y
+D=X-Y
+E=X*Y
+F=X/Y
+print('Tổng của', X, '+', Y, '=', C)
+print('Hiệu của', X, '-', Y, '=', D)
+print('Tích của', X, '*', Y, '=', E)
+print('Thương của', X, '/', Y, '=', F)
+
+
+#bài tập 1
+print("**    **")
+print("**    **")
+print("********")
+print("**    **")
+print("**    **")
+
+#bài tập 2
+X=10
+Y=5
+C=X+Y
+D=X-Y
+E=X*Y
+F=X/Y
+print('Tổng của', X, '+', Y, '=', C)
+print('Hiệu của', X, '-', Y, '=', D)
+print('Tích của', X, '*', Y, '=', E)
+print('Thương của', X, '/', Y, '=', F)
+
+#bài tập 3
+ten_hang="sữa hộp vina milk"
+so_luong=5
+don_gia=25000
+tien_phai_tra=so_luong*don_gia
+print(ten_hang, '=', tien_phai_tra )
+
+
+
+
